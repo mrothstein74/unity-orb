bf4820427b7fdd47a0eb45ee6745609b48db028b
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-25 20:48:20 -0700

    fix editor version

--- src/scripts/prepare-env.sh
+++ src/scripts/prepare-env.sh
@@ -6,10 +6,10 @@ readonly unity_project_full_path="$base_dir/$PARAM_PROJECT_PATH"
 install_unity_windows() {
   # The piped empty space is required to prevent the script from hanging indefinitely.
   # https://serverfault.com/a/932946
-  printf '%s\n' "" | choco install unity-hub
+  choco install unity-hub
 
   # shellcheck disable=SC2140
-  /c/"Program Files"/"Unity Hub"/"Unity Hub.exe" -- --headless install --version "2022" --module windows-il2cpp --childModules
+  /c/"Program Files"/"Unity Hub"/"Unity Hub.exe" -- --headless install --version "2021" --module windows-il2cpp --childModules
 }
 
 download_before_script() {
