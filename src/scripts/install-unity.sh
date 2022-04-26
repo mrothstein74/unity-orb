ba6ae3734c2fe992248119a9dcaf72931a20722a
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-25 21:50:15 -0700

    fix checksum

--- src/scripts/install-unity.sh
+++ src/scripts/install-unity.sh
@@ -3,7 +3,7 @@
 install_unity_windows() {
   # The piped empty space is required to prevent the script from hanging indefinitely.
   # https://serverfault.com/a/932946
-  printf '%s\n' "" | choco install unity-hub --version 2.4.5.20210724
+  printf '%s\n' "" | choco install unity-hub --version 3.1.2 --checksum64 B12C5390400243B52F5F37153087F7669EDC25A56F282C0A5835B76B9EF61771
   printf '%s\n' 'PATH=$PATH:/c/Program Files/Unity Hub' >> "$BASH_ENV"
   
   # shellcheck source=/dev/null
