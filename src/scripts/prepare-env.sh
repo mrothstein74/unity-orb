2fdf4437cda4f4c488c81472a76351db6eb77ca5
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-25 21:33:53 -0700

    add hub to path

--- src/scripts/prepare-env.sh
+++ src/scripts/prepare-env.sh
@@ -7,9 +7,12 @@ install_unity_windows() {
   # The piped empty space is required to prevent the script from hanging indefinitely.
   # https://serverfault.com/a/932946
   printf '%s\n' "" | choco install unity-hub
-
+  printf '%s\n' "PATH=$PATH:/c/Program Files/Unity Hub" >> "$BASH_ENV"
+  
+  source "$BASH_ENV"
+  
   # shellcheck disable=SC2140
-  /c/"Program Files"/"Unity Hub"/"Unity Hub.exe" -- --headless install --version "2021" --module windows-il2cpp --childModules
+  "Unity Hub.exe" -- --headless install --version "2021" --module windows-il2cpp --childModules
 }
 
 download_before_script() {
