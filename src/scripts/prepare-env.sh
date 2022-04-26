663a892ac2833c0fefb77101421642befeb2ef41
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-25 20:16:30 -0700

    feed newline to fix choco error

--- src/scripts/prepare-env.sh
+++ src/scripts/prepare-env.sh
@@ -4,8 +4,8 @@ readonly base_dir="${CIRCLE_WORKING_DIRECTORY/\~/$HOME}"
 readonly unity_project_full_path="$base_dir/$PARAM_PROJECT_PATH"
 
 install_unity_windows() {
-  choco install -y unity-hub
-  
+  printf '\n' "" | choco install -y unity-hub
+
   # shellcheck disable=SC2140
   /c/"Program Files"/"Unity Hub"/"Unity Hub.exe" -- --headless install --version "2021" --module windows-il2cpp --childModules
 }
